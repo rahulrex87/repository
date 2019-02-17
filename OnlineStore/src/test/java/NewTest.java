02527b130ac274124a88676b338976739590639a
rahul <rahul@DESKTOP-437EQGF>
2019-02-17 16:23:23 +0530

    commit

---
+++ OnlineStore/src/test/java/NewTest.java
@@ -0,0 +1,10 @@
+package test.java;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.print("H");
+  }
+}
---
+++ OnlineStore/src/test/java/NewTest2.java
@@ -0,0 +1,9 @@
+package test.java;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void g() {
+  }
+}
