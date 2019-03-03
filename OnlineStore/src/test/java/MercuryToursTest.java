85d302cada469d4f907ea065b47ee337e3b2c185
rahulrex87 <rahulco2003@yahoo.co.in>
2019-03-03 11:56:29 +0530

    Master Branch
    Master Branch

--- OnlineStore/src/test/java/MercuryToursTest.java
+++ OnlineStore/src/test/java/MercuryToursTest.java
@@ -12,10 +12,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 public class MercuryToursTest {
     WebDriver driver;
